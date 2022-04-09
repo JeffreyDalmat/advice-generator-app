3c33c8b493c26e147d93dda55ba12765f30b92ee
Jeffwie <dalmat.jeffrey@gmail.com>
2022-04-09 19:43:51 +0200

    fix: responsive

--- src/components/Divider/index.js
+++ src/components/Divider/index.js
@@ -1,36 +1,14 @@
+import './divider.scss';
+import dividerMobile from 'src/assets/images/pattern-divider-mobile.svg';
+import dividerDesktop from 'src/assets/images/pattern-divider-desktop.svg';
+
 export default function Divider() {
   return (
-    <svg
-      width="444"
-      height="16"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <g
-        fill="none"
-        fillRule="evenodd"
-      >
-        <path
-          fill="hsl(217, 19%, 38%)"
-          d="M0 8h196v1H0zM248 8h196v1H248z"
-        />
-        <g
-          transform="translate(212)"
-          fill="#CEE3E9"
-        >
-          <rect
-            width="6"
-            height="16"
-            rx="3"
 
-          />
-          <rect
-            x="14"
-            width="6"
-            height="16"
-            rx="3"
-          />
-        </g>
-      </g>
-    </svg>
+    <>
+      <img className="divider-mobile" src={dividerMobile} alt="" />
+      <img className="divider-desktop" src={dividerDesktop} alt="" />
+    </>
+
   );
 }
